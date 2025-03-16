e0aa5fe098c1a3baed5c309a8ea47f165826d75b
Dev-SarthakBora <borasarthak275@gmail.com>
2025-03-16 12:37:26 +0530

    Delete website directory

--- website/models.py
+++
@@ -1,8 +0,0 @@
-
-from flask_sqlalchemy import SQLAlchemy 
-from datetime import datetime
-from . import db
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(150), nullable=False)
-    date = db.Column(db.DateTime(timezone=True), default=datetime.today()) 
